80ef0bdd451184cc53f3ef76379669a354abb176
danilojoncic <djoncic14122rn@raf.rs>
2024-05-23 20:55:53 +0200

    added cors

---
+++ src/main/java/com/example/touristguide/service/CorsFilter.java
@@ -0,0 +1,21 @@
+package com.example.touristguide.service;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        //nije sjajno imati * ali za potrebe projekta neka ostane
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "*");
+    }
+}
